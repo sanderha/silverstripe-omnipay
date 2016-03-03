714071da23670e643abcdb69858a49bd9c83ac4d
sanderha <sander.ha@hotmail.com>
2016-03-03 14:58:16 +0100

    Update RefundMessages.php

--- code/model/messaging/RefundMessages.php
+++ code/model/messaging/RefundMessages.php
@@ -3,6 +3,9 @@
 class RefundRequest extends GatewayRequestMessage{
 	
 }
+class CompleteRefundRequest extends GatewayRequestMessage{
+
+}
 class RefundedResponse extends GatewayResponseMessage{
 	
 }
